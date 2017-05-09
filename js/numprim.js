d7e1478ae15a17a7f8e19cbf49096d6c8b31e636
Zomboyde <Zomboyde>
2017-05-09 01:34:06 -0500

    Primer Commit :v

---
+++ js/numprim.js
@@ -0,0 +1,13 @@
+
+function esPrimo(int numero){
+  var contador = 2;
+  boolean primo=true;
+  while ((primo) && (contador!=numero)){
+    if (numero % contador == 0)
+      primo = false;
+    contador++;
+  }
+  return aprimos.join();
+}
+
+alert(primos(1000));
